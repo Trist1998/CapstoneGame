5e020169d84d932ae5fc68d38c8418b955453c8a
Trist1998 <tristanmwood@hotmail.com>
2019-10-07 13:59:58 +0200

    Changed grenade models and added frag grenade

--- Capstone/Assets/scripts/weapons/spells/aquio/SpellWater.cs
+++ Capstone/Assets/scripts/weapons/spells/aquio/SpellWater.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpellWater : AbstractProjectileWeaponEffect
 {
     public readonly string SPELL_NAME = "Aquio (The Holy Water Potion)";
-
+    public float radius;
     public override void processPrimaryHit(Item item, GameObject hit, Vector3 hitPoint, Vector3 direction)
     {
-        WetEffect effect = hit.AddComponent<WetEffect>();
-        effect.startEffect(lifeTime);
-        playPrimaryOnHitEffect(hit, hitPoint);
+        
+        Collider[] objects = Physics.OverlapSphere(hitPoint, radius, LayerMask.GetMask("Destructable"));
+        foreach (var colliderObject in objects)
+        {
+            if (colliderObject.gameObject.GetComponent<WetEffect>() != null) break;
+            WetEffect effect = colliderObject.gameObject.AddComponent<WetEffect>();
+            effect.startEffect(lifeTime);
+        }
+        base.processPrimaryHit(item, hit, hitPoint, direction);
     }
 
     public override string getName()
